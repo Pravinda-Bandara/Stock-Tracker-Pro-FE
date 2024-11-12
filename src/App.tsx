60517c32d90fa11b9afd6b9f6c29f57a2e8fea3a
Pravinda-Bandara <bandarapravinda@gmail.com>
2024-11-13 00:07:32 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router";
+import Navbar from "./Components/Navbar/Navbar";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
+import { ToastContainer } from "react-toastify";
+import { UserProvider } from "./Context/useAuth";
+
+function App() {
+  return (
+    <>
+      <h1>asd</h1>
+    </>
+  );
+}
+
+export default App;
