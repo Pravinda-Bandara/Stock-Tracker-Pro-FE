bb8c6f564203010af79b3e81cd5a973e0dbd57fd
Pravinda-Bandara <bandarapravinda@gmail.com>
2024-11-14 00:29:16 +0530

    add navBar ,tailwind,images

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { Outlet } from "react-router";
 import Navbar from "./Components/Navbar/Navbar";
 import "react-toastify/dist/ReactToastify.css";
-import "./App.css";
 import { ToastContainer } from "react-toastify";
 import { UserProvider } from "./Context/useAuth";
 
 function App() {
   return (
     <>
-      <h1>asd</h1>
+      <UserProvider>
+        <Navbar />
+        <Outlet />
+        <ToastContainer />
+      </UserProvider>
     </>
   );
 }
