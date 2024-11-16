7e6a4e81ebcf52d1145b0ec897736a9652b60750
Pravinda-Bandara <bandarapravinda@gmail.com>
2024-11-16 18:41:42 +0530

    add animation

--- src/Components/Portfolio/CardPortfolio/CardPortfolio.tsx
+++ src/Components/Portfolio/CardPortfolio/CardPortfolio.tsx
@@ -10,15 +10,17 @@ interface Props {
 
 const CardPortfolio = ({ portfolioValue, onPortfolioDelete }: Props) => {
   return (
-    <div className="flex flex-col items-center p-6 space-y-4 text-center bg-white rounded-lg shadow-md">
-      <div className="text-2xl font-semibold text-gray-800">
-        {portfolioValue.symbol}
+    <div className="flex flex-col items-center justify-between p-6 space-y-4 text-center bg-white rounded-lg border border-gray-300 bg-gray-100 hover:scale-105 transition-transform duration-200 ease-in-out">
+      <div className="text-sm font-semibold text-gray-800 sm:text-lg">
+        {portfolioValue.companyName}
       </div>
-      <DetailsPortfolio portfolioValue={portfolioValue.symbol} />
-      <DeletePortfolio
-        portfolioValue={portfolioValue.symbol}
-        onPortfolioDelete={onPortfolioDelete}
-      />
+      <div className="flex flex-col sm:flex-row sm:space-x-4 sm:space-y-0 space-y-4">
+        <DetailsPortfolio portfolioValue={portfolioValue.companyName} />
+        <DeletePortfolio
+          portfolioValue={portfolioValue.companyName}
+          onPortfolioDelete={onPortfolioDelete}
+        />
+      </div>
     </div>
   );
 };
