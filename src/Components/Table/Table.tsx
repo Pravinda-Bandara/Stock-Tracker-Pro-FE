416cb98f7ed4863e109a777d2606d541fa28135d
Pravinda-Bandara <bandarapravinda@gmail.com>
2024-11-18 23:59:51 +0530

    change table text alignment

--- src/Components/Table/Table.tsx
+++ src/Components/Table/Table.tsx
@@ -8,29 +8,36 @@ const Table = ({ config, data }: Props) => {
     return (
       <tr key={company.cik}>
         {config.map((val: any) => {
-          return <td className="p-3">{val.render(company)}</td>;
+          return (
+            <td className="p-3 text-center">
+              {val.render(company)}
+            </td>
+          );
         })}
       </tr>
     );
   });
+
   const renderedHeaders = config.map((config: any) => {
     return (
       <th
-        className="p-4 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+        className="p-4 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
         key={config.label}
       >
         {config.label}
       </th>
     );
   });
+
   return (
     <div className="bg-white shadow rounded-lg m-4 mb-4 p-4 sm:p-6 w-full">
-  <table className="min-w-full divide-y divide-gray-200 m-5">
-    <thead className="bg-gray-50">{renderedHeaders}</thead>
-    <tbody>{renderedRows}</tbody>
-  </table>
-</div>
-
+      <table className="min-w-full divide-y divide-gray-200 m-5">
+        <thead className="bg-gray-50">
+          <tr>{renderedHeaders}</tr>
+        </thead>
+        <tbody>{renderedRows}</tbody>
+      </table>
+    </div>
   );
 };
 
