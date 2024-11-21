09d5ce9f9d6cadb8960a14c5fae36d10e6f9569f
Pravinda-Bandara <bandarapravinda@gmail.com>
2024-11-21 11:32:14 +0530

    sidebar active style

--- src/Components/Portfolio/CardPortfolio/CardPortfolio.tsx
+++ src/Components/Portfolio/CardPortfolio/CardPortfolio.tsx
@@ -11,7 +11,7 @@ interface Props {
 const CardPortfolio = ({ portfolioValue, onPortfolioDelete }: Props) => {
   return (
     <div className="flex flex-col items-center justify-between p-6 space-y-4 text-center bg-white rounded-lg border border-gray-300 bg-bgGray9 hover:scale-105 transition-transform duration-200 ease-in-out">
-      <div className="text-sm font-semibold text-textGray4 sm:text-lg">
+      <div className="text-sm font-semibold text-textGray4 sm:text-lg text-black">
         {portfolioValue.companyName}
       </div>
       <div className="flex flex-col sm:flex-row sm:space-x-4 sm:space-y-0 space-y-4">
--- src/Components/Sidebar/Sidebar.tsx
+++ src/Components/Sidebar/Sidebar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { FaHome, FaTable, FaMoneyBill, FaBriefcase } from "react-icons/fa";
 import { FaTableCells } from "react-icons/fa6";
@@ -7,6 +7,16 @@ import { SlGraph } from "react-icons/sl";
 type Props = {};
 
 const Sidebar = (props: Props) => {
+  const [activeTab, setActiveTab] = useState<string>("Company Profile");
+
+  const handleTabClick = (tab: string) => {
+    setActiveTab(tab);
+  };
+
+  // Centralized active styles
+  const activeStyles = "text-textWhite";
+
+
   return (
     <nav className="fixed md:block py-4 px-6 mt-16 top-0 bottom-0 w-56 bg-bgBlack left-0 transition-all duration-300 ease-in-out transform md:translate-x-0 -translate-x-full h-full">
       <div className="flex-col min-h-full px-0 pt-7 flex flex-wrap items-center justify-between w-full mx-auto overflow-y-auto">
@@ -14,42 +24,60 @@ const Sidebar = (props: Props) => {
           <div className="md:flex-col md:min-w-full flex flex-col list-none">
             <Link
               to="/search"
-              className="flex md:min-w-full text-textGreen4 text-xs uppercase font-bold block pt-1 pb-4 no-underline"
+              onClick={() => handleTabClick("Portfolio")}
+              className={`flex md:min-w-full ${
+                activeTab === "Portfolio" ? activeStyles : "text-textGreen4"
+              } text-xs uppercase block pt-1 pb-4 no-underline`}
             >
               <FaBriefcase />
               <h6 className="ml-3">Portfolio</h6>
             </Link>
             <Link
               to="company-profile"
-              className="flex md:min-w-full text-textGreen4 text-xs uppercase font-bold block pt-1 pb-4 no-underline"
+              onClick={() => handleTabClick("Company Profile")}
+              className={`flex md:min-w-full ${
+                activeTab === "Company Profile" ? activeStyles : "text-textGreen4"
+              } text-xs uppercase block pt-1 pb-4 no-underline`}
             >
               <FaHome />
               <h6 className="ml-3">Company Profile</h6>
             </Link>
             <Link
               to="income-statement"
-              className="flex md:min-w-full text-textGreen4 text-xs uppercase font-bold block pt-1 pb-4 no-underline"
+              onClick={() => handleTabClick("Income Statement")}
+              className={`flex md:min-w-full ${
+                activeTab === "Income Statement" ? activeStyles : "text-textGreen4"
+              } text-xs uppercase block pt-1 pb-4 no-underline`}
             >
               <FaTable />
               <h6 className="ml-3">Income Statement</h6>
             </Link>
             <Link
               to="balance-sheet"
-              className="flex md:min-w-full text-textGreen4 text-xs uppercase font-bold block pt-1 pb-4 no-underline"
+              onClick={() => handleTabClick("Balance Sheet")}
+              className={`flex md:min-w-full ${
+                activeTab === "Balance Sheet" ? activeStyles : "text-textGreen4"
+              } text-xs uppercase block pt-1 pb-4 no-underline`}
             >
               <FaTableCells />
               <h6 className="ml-3">Balance Sheet</h6>
             </Link>
             <Link
               to="cashflow-statement"
-              className="flex md:min-w-full text-textGreen4 text-xs uppercase font-bold block pt-1 pb-4 no-underline"
+              onClick={() => handleTabClick("Cashflow Statement")}
+              className={`flex md:min-w-full ${
+                activeTab === "Cashflow Statement" ? activeStyles : "text-textGreen4"
+              } text-xs uppercase block pt-1 pb-4 no-underline`}
             >
               <FaMoneyBill />
               <h6 className="ml-3">Cashflow Statement</h6>
             </Link>
             <Link
               to="historical-dividend"
-              className="flex md:min-w-full text-textGreen4 text-xs uppercase font-bold block pt-1 pb-4 no-underline"
+              onClick={() => handleTabClick("Historical Dividend")}
+              className={`flex md:min-w-full ${
+                activeTab === "Historical Dividend" ? activeStyles : "text-textGreen4"
+              } text-xs uppercase block pt-1 pb-4 no-underline`}
             >
               <SlGraph />
               <h6 className="ml-3">Historical Dividend</h6>
