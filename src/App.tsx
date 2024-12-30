3473fcdfbf2d3905c7f7da12919db2c4440e7a61
Pravinda-Bandara <bandarapravinda@gmail.com>
2024-12-31 03:19:37 +0530

    use interceptor

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import { Outlet } from "react-router";
+import { Outlet, useNavigate } from "react-router";
 import Navbar from "./Components/Navbar/Navbar";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 import { UserProvider } from "./Context/useAuth";
 import { useEffect } from "react";
-import setupAxiosInterceptor from "./Services/AxiosInterceptor";
+import { setupInterceptors } from "./Services/AxiosInterceptor";
+
 
 function App() {
+  const navigate = useNavigate();
   useEffect(() => {
-    setupAxiosInterceptor();
-  }, []);
+    setupInterceptors(navigate);
+}, [navigate]);
   return (
     <>
       <div className="min-h-screen bg-bgGray9">
